82349b15a4c475a8b0c49ea489e8b45a41aa4aee
Admin 007 <Admin 007@DESKTOP-22FBA6V>
2023-08-08 21:24:07 +0530

    CRUD

--- Simple-1/src/main/java/com/example/demo/Controller/Usercontroller.java
+++ Simple-1/src/main/java/com/example/demo/Controller/Usercontroller.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,4 +30,30 @@ public class Usercontroller {
 	@GetMapping("/users")
 	public List<Userentity> getAllUsers() {
 		return obj.findAll();
-} }
+}
+	@PutMapping("/update")
+	public Userentity updateuser (@RequestBody Userentity up) {
+		try {
+			obj.save(up);
+			return up ;
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return up;
+	}
+@DeleteMapping("/getdel/{Id}")
+public String del (@PathVariable int Id) {
+try {
+	@SuppressWarnings("deprecation")
+
+	Userentity up=obj.getOne (Id);
+	obj.delete(up);
+	return "delete";
+}
+catch(Exception ex) {
+	ex.printStackTrace();
+}
+return null;
+}	
+}
--- Simple-1/src/main/java/com/example/demo/Repository/Userrepo.java
+++ Simple-1/src/main/java/com/example/demo/Repository/Userrepo.java
@@ -3,5 +3,5 @@ import com.example.demo.Entity.Userentity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface Userrepo extends JpaRepository<Userentity,String> {
+public interface Userrepo extends JpaRepository<Userentity,Integer> {
 }
